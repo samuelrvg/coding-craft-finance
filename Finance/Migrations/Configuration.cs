41973a4ba306de7455f54f69fe6d8c3ee89c4b0f
samuel.rvg <samuel.rvg@gmail.com>
2020-01-12 00:46:41 -0300

    migration seed

--- Finance/Migrations/Configuration.cs
+++ Finance/Migrations/Configuration.cs
@@ -15,6 +15,21 @@
 
         protected override void Seed(Finance.Models.ApplicationDbContext context)
         {
+            context.DespesaCategorias.AddOrUpdate(
+                new Models.DespesaCategoria() { Nome = "Alimentação"},
+                new Models.DespesaCategoria() { Nome = "Casa" }
+                );
+
+            context.ReceitaCategorias.AddOrUpdate(
+                new Models.ReceitaCategoria() { Nome = "Salário" },
+                new Models.ReceitaCategoria() { Nome = "Outros" }
+                );
+
+            context.Bancos.AddOrUpdate(
+                new Models.Banco() { Nome = "Bradesco" },
+                new Models.Banco() { Nome = "Nubank" }
+                );
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
